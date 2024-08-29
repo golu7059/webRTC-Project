e667b856aae8e326d7b06e173e62f500201af093
golu Kumar <golukumarjaisidih@gmail.com>
2024-08-29 23:50:39 +0530

    general socket io completed

--- server/index.js
+++ server/index.js
@@ -1,13 +1,31 @@
 const express = require('express');
+const path = require('path');
+const http = require('http');
+const { Server } = require('socket.io');
 
 const app = express();
+
+// make server using http 
+const server = http.createServer(app);
+const io = new Server(server);
 const PORT = process.env.PORT || 5065;
 
+// to use static files like html files and we are usign path module so it should be resolved
+app.use(express.static(path.resolve('./public')));
+
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.sendFile('./public/index.html')
 })
 
-app.listen(PORT, () => {
+// socket.io connection 
+io.on('connection', (socket) => { // socket is basically client information / data
+    socket.on("chat-message",(message) => {
+        console.log(`sockedt id ${socket.id} send : `,message);
+        io.emit('server-message',message);
+    })
+})
+
+server.listen(PORT, () => {
     console.log(`Example app listening at http://localhost:${PORT}`)
 }).on("error", (e) => {
     if (e.code == "EADDRINUSE") {

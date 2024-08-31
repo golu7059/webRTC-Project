7898479d0f9e96879e94c11ecb0369f9f1cf129b
golu Kumar <golukumarjaisidih@gmail.com>
2024-09-01 02:35:29 +0530

    added auth routing

--- chat-room/src/App.js
+++ chat-room/src/App.js
@@ -1,72 +1,22 @@
-import React, { useState, useEffect, useRef } from "react";
-import io from "socket.io-client";
-import { Button, Container, TextField, Typography } from "@mui/material";
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Signup from './pages/Register';
+import Chat from './pages/Chat';
+import './index.css';  // Import global styles
 
-function App() {
-  const [message, setMessage] = useState("");
-  const [messages, setMessages] = useState([]); // State to hold received messages
-  const socketRef = useRef();
-
-  useEffect(() => {
-    // Initialize socket connection
-    socketRef.current = io("http://localhost:5065");
-
-    // Log connection
-    socketRef.current.on("connect", () => {
-      console.log("Connected");
-    });
-
-    // Listen for incoming messages
-    socketRef.current.on("message", (data) => {
-      setMessages((prevMessages) => [...prevMessages, data]); // Append new message to messages array
-      console.log(data); // Log the message
-    });
-
-    // Clean up on component unmount
-    return () => {
-      socketRef.current.disconnect();
-    };
-  }, []); // Empty dependency array ensures this runs only once
-
-  const sendMessage = (e) => {
-    e.preventDefault();
-    socketRef.current.emit("message", message); // Emit message to server
-    setMessage(""); // Clear input field
-  };
-
+const App = () => {
   return (
-    <div>
-      <Container maxWidth="sm">
-        <Typography variant="h1" component="div" gutterBottom>
-          Chat Room
-        </Typography>
-
-        {/* Display received messages */}
-        <div>
-          {messages.map((msg, index) => (
-            <Typography key={index} variant="body1">
-              {msg}
-            </Typography>
-          ))}
-        </div>
-
-        <form onSubmit={sendMessage}>
-          <TextField
-            value={message}
-            onChange={(e) => setMessage(e.target.value)}
-            id="outlined-basics"
-            label="Message"
-            variant="outlined"
-            fullWidth
-          />
-
-          <Button type="submit" variant="contained" color="primary">
-            Send
-          </Button>
-        </form>
-      </Container>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login/>} />
+        <Route path="/signup" element={<Signup/>} />
+        <Route path="/chat"  element={<Chat/>} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;

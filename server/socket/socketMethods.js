ca4ac84861a9fb992d92440c411e9eb8cc5cbb25
golu Kumar <golukumarjaisidih@gmail.com>
2024-09-01 10:33:47 +0530

    Added socket folder config and methods

---
+++ server/socket/socketMethods.js
@@ -0,0 +1,40 @@
+import io from './socketConfig'
+
+io.on("connection", (socket) => {
+    console.log(`User connected: ${socket.id}`);
+  
+    socket.on("join", (userId) => {
+      users[userId] = socket.id;
+      console.log("Users online:", users);
+    });
+  
+    socket.on("sendMessage", async (data) => {
+      const { senderId, receiverId, message, media } = data;
+  
+      const chatMessage = new Chat({ sender: senderId, receiver: receiverId, message, media });
+      await chatMessage.save();
+  
+      const receiverSocketId = users[receiverId];
+      if (receiverSocketId) {
+        io.to(receiverSocketId).emit("getMessage", chatMessage);
+      }
+    });
+  
+    socket.on("callUser", ({ userToCall, signalData, from, name }) => {
+      io.to(users[userToCall]).emit("callUser", { signal: signalData, from, name });
+    });
+  
+    socket.on("answerCall", (data) => {
+      io.to(users[data.to]).emit("callAccepted", data.signal);
+    });
+  
+    socket.on("disconnect", () => {
+      for (const userId in users) {
+        if (users[userId] === socket.id) {
+          delete users[userId];
+          break;
+        }
+      }
+      console.log(`User disconnected: ${socket.id}`);
+    });
+  });
